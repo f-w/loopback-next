0833be5ce1c11a7f433fe595950577e7dde2fa5a
Raymond Feng <enjoyjava@gmail.com>
2018-09-26 15:58:46 -0700

    feat(rest): sort routes based on verb/path to match better

--- packages/rest/src/router/routing-table.ts
+++ packages/rest/src/router/routing-table.ts
@@ -18,6 +18,7 @@ import {
   ValueOrPromise,
 } from '@loopback/context';
 import * as HttpErrors from 'http-errors';
+import * as pathToRegExp from 'path-to-regexp';
 import {inspect} from 'util';
 
 import {
@@ -127,6 +128,8 @@ export class RoutingTable {
       );
     }
     this._routes.push(route);
+    // Sort routes
+    this._routes.sort(compareRoute);
   }
 
   describeApiPaths(): PathObject {
@@ -247,7 +250,12 @@ export abstract class BaseRoute implements RouteEntry {
   }
 
   match(request: Request): ResolvedRoute | undefined {
-    debug('trying endpoint %s', inspect(this, {depth: 5}));
+    debug(
+      'trying endpoint %s for %s %s',
+      inspect(this, {depth: 5}),
+      request.method,
+      request.path,
+    );
     if (this.verb !== request.method!.toLowerCase()) {
       debug(' -> verb mismatch');
       return undefined;
@@ -468,4 +476,74 @@ export function createControllerFactoryForInstance<T>(
   controllerInst: T,
 ): ControllerFactory<T> {
   return ctx => controllerInst;
+}
+
+const HTTP_VERBS: {[name: string]: number} = {
+  post: 1,
+  put: 2,
+  patch: 3,
+  get: 4,
+  head: 5,
+  delete: 6,
+  options: 7,
+};
+
+/**
+ * Compare two routes by verb/path for sorting
+ * @param route1 First route entry
+ * @param route2 Second route entry
+ */
+export function compareRoute(
+  route1: Pick<RouteEntry, 'verb' | 'path'>,
+  route2: Pick<RouteEntry, 'verb' | 'path'>,
+) {
+  // First check verb
+  const verb1 = HTTP_VERBS[route1.verb.toLowerCase()] || 3;
+  const verb2 = HTTP_VERBS[route2.verb.toLowerCase()] || 3;
+  if (verb1 !== verb2) return verb1 - verb2;
+
+  // Then check the path tokens
+  const path1 = route1.path.replace(/{([^}]*)}(\/|$)/g, ':$1$2');
+  const path2 = route2.path.replace(/{([^}]*)}(\/|$)/g, ':$1$2');
+  const tokensForPath1: pathToRegExp.Token[] = parse(path1);
+  const tokensForPath2: pathToRegExp.Token[] = parse(path2);
+
+  // Longer path comes before shorter path
+  if (tokensForPath1.length < tokensForPath2.length) {
+    return 1;
+  } else if (tokensForPath1.length > tokensForPath2.length) {
+    return -1;
+  }
+
+  // Now check token by token
+  for (let i = 0; i < tokensForPath1.length; i++) {
+    const token1 = tokensForPath1[i];
+    const token2 = tokensForPath2[i];
+    if (typeof token1 === 'string' && typeof token2 === 'string') {
+      if (token1 < token2) return -1;
+      else if (token1 > token2) return 1;
+    } else if (typeof token1 === 'string' && typeof token2 === 'object') {
+      // token 1 is a literal while token 2 is a parameter
+      return -1;
+    } else if (typeof token1 === 'object' && typeof token2 === 'string') {
+      // token 1 is a parameter while token 2 is a literal
+      return 1;
+    } else {
+      // Both token are parameters. Treat as equal weight.
+    }
+  }
+  return 0;
+}
+
+function parse(path: string) {
+  const tokens: pathToRegExp.Token[] = [];
+  pathToRegExp.parse(path).forEach(p => {
+    if (typeof p === 'string') {
+      // The string can be /orders/count
+      tokens.push(...p.split('/').filter(Boolean));
+    } else {
+      tokens.push(p);
+    }
+  });
+  return tokens;
 }
